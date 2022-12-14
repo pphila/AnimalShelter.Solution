2464c00fd9adae1add66013cdfa57a13da025e33
pphila <efl2ice89@gmail.com>
2022-12-13 20:50:09 -0800

    add all dependencies and boilerplate to project file and started working on animal shelter link to MySql

---
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Items.ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.animalId == id);
+      return View(thisAnimal);
+    }
+  }
+}
